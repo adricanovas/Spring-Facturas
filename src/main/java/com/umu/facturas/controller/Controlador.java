b456fa562125d5c950dbefa66879b7f6783fff05
Adri <adrian.canovas1@um.es>
2021-11-15 00:38:07 +0100

    Eliminar y crear facturas

--- src/main/java/com/umu/facturas/controller/Controlador.java
+++ src/main/java/com/umu/facturas/controller/Controlador.java
@@ -1,6 +1,7 @@
 package com.umu.facturas.controller;
 
 import com.umu.facturas.model.Factura;
+import com.umu.facturas.model.Producto;
 import com.umu.facturas.repository.FacturaRepository;
 import com.umu.facturas.repository.ProductoRepository;
 
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,17 +27,40 @@ public class Controlador {
 		return "index";
 	}
 
-	@RequestMapping("/form")
-	public String form(Model model, Boolean edit) {
-		model.addAttribute("editable", edit);
+	@RequestMapping("/nueva")
+	public String nuevaFactura(Model model) {
+		boolean block_edit = false;
+		model.addAttribute("editable", block_edit);
         model.addAttribute("factura", new Factura());
 		model.addAttribute("productos", productoRep.findAll());
 		return "form";
 	}
 
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	@RequestMapping("/editar/{id}")
+	public String editarFactura(Model model, @PathVariable Integer id) {
+		boolean block_edit = true;
+		model.addAttribute("editable", block_edit);
+		model.addAttribute("factura", facturaRep.findById(id).get());
+		model.addAttribute("productos", productoRep.findAll());
+		return "form";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST, params={"save"})
 	public String saveFactura(@ModelAttribute("factura") Factura factura){
+		//DEBUG:
+		System.out.println(factura.getId());
 		facturaRep.save(factura);
+		return "redirect:/";
+	}
+	@RequestMapping(value = "/save", method = RequestMethod.POST, params={"addProduct"})
+	public String addProduct(@ModelAttribute("producto") Producto producto, Model model){
+		
+		return "redirect:/";
+	}
+
+	@RequestMapping(value = "/delete/{id}")
+	public String deleteFactura(@PathVariable Integer id, Model model){
+		facturaRep.deleteById(id);
 		return "redirect:/";
 	}
 }
